4c24a169753f2fed17b573a9eed5e4b527f791ca
jdnbo <jay.boychuk@mail.utoronto.ca>
2015-04-17 19:02:58 +0000

    create chefs and recipes table

---
+++ db/migrate/20150417174955_add_title_and_description_to_recipes.rb
@@ -0,0 +1,6 @@
+class AddTitleAndDescriptionToRecipes < ActiveRecord::Migration
+  def change
+    add_column :recipes, :title, :string
+    add_column :recipes, :description, :string
+  end
+end
---
+++ db/migrate/20150417175320_add_summary_and_timestamps_to_recipes.rb
@@ -0,0 +1,7 @@
+class AddSummaryAndTimestampsToRecipes < ActiveRecord::Migration
+  def change
+    add_column :recipes, :summary, :text
+    add_column :recipes, :created_at, :datetime
+    add_column :recipes, :updated_at, :datetime
+  end
+end
