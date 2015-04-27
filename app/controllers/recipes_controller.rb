66090615710e74df350a77b5fc85e9f923aeffea
jdnbo <jay.boychuk@mail.utoronto.ca>
2015-04-27 16:29:52 +0000

    add image uploader

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -42,7 +42,7 @@ class RecipesController < ApplicationController
   private
   
     def recipe_params
-      params.require(:recipe).permit(:title, :summary, :description)
+      params.require(:recipe).permit(:title, :summary, :description, :picture)
     end
   
 end
