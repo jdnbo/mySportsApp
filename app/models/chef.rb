01e920d1b66d33a95a94bb6f86db646a060a0b47
jdnbo <jay.boychuk@mail.utoronto.ca>
2015-04-28 18:51:48 +0000

    added pagination

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,6 @@
 class Chef < ActiveRecord::Base
   has_many :recipes
+  has_many :likes
   before_save { self.email = email.downcase } #before the email is saved it converts it all to lowercase
   validates :chefname, presence: true, length: { minimum: 3, maximum: 40 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #tell rails you have a constant using all upper case letters
