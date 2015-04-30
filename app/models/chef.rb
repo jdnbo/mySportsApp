9f2746899a70bed2a7653da24dae22d8a56ce9fa
jdnbo <jay.boychuk@mail.utoronto.ca>
2015-04-30 17:36:50 +0000

    create chefs resources and add authentication

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -6,4 +6,5 @@ class Chef < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #tell rails you have a constant using all upper case letters
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }, format: { with: VALID_EMAIL_REGEX }
   
+  has_secure_password
 end
