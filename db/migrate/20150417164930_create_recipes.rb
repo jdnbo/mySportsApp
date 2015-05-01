18e63a345ff082cb2eda689d8176094386429425
jdnbo <jay.boychuk@mail.utoronto.ca>
2015-05-01 21:22:55 +0000

    fixed migration error

--- db/migrate/20150417164930_create_recipes.rb
+++ db/migrate/20150417164930_create_recipes.rb
@@ -1,7 +1,7 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
-      t.string :name
+      t.string :title
       t.text :summary
       t.text :description
       t.timestamps
