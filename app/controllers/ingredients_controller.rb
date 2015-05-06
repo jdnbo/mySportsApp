aedbc570ae8cb157c0991761d97bf526e56fc012
jdnbo <jay.boychuk@mail.utoronto.ca>
2015-05-06 14:46:36 +0000

    Create styles and ingredients associations

---
+++ app/controllers/ingredients_controller.rb
@@ -0,0 +1,31 @@
+class IngredientsController < ApplicationController
+  before_action :require_login, except: [:show]
+  
+  def show
+    @ing = Ingredient.find(params[:id])
+    @recipes = @ing.recipes.paginate(page: params[:page], per_page: 4)
+  end
+  
+  def new
+    @ing = Ingredient.new
+  end
+  
+  def create
+    @ing = Ingredient.new(ing_params)
+    
+    if @ing.save
+      flash[:success] = "Your ingredient was created succesfully"
+      redirect_to recipes_path
+    else
+      render :new
+    end
+      
+  end
+  
+  private
+  
+  def ing_params
+    params.require(:ingredient).permit(:name)
+  end
+  
+end
